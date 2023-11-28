dddbc27a52ef14294a17f7ea5a8a1827e1c45153
Martin Rodriguez <neomartinr@gmail.com>
2023-11-27 21:44:47 -0300

    Weather widget funcionando con el buscador y geolocalizacion

--- js/weather.js
+++ js/weather.js
@@ -2,18 +2,95 @@ const KEY = `45a75d3ff81ec473ba7a63887c4c3648`
 
 const lat = '30.064385'
 const lon = '31.233401'
+const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${KEY}&units=metric`;
 
-const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}&units=metric`
+const inputWeather = document.querySelector('#weather-search')
+const searchBtn = document.getElementById('weather-search-btn')
 
-axios.get(URL)
-        .then(respuesta => {
-            console.log(respuesta.data)
-        })
-        .catch(error => console.log(error))  
+const climaNombre = document.getElementById('weather-name')
+const climaTemp = document.getElementById('weather-temp');
+const climaIcon = document.getElementById('weather-icon')
+
+function obtenerClimaPorCoordenadas(lat, lon) {
+
+    const parametros = `&lat=${lat}&lon=${lon}`
+    
+    axios.get(URL+parametros)
+    .then(respuesta => {
+        pintarCardClima(respuesta.data)
+    })
+    .catch(error => console.log(error))  
+}
+
+function localizacionInicial() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (data) => {
+                const latitud = data.coords.latitude;
+                const longitud = data.coords.longitude;
+
+                obtenerClimaPorCoordenadas(latitud, longitud)
+
+
+            }, (error) => {
+                obtenerClimaPorCoordenadas(lat, lon);
+                console.warn(`No se pudo obtener la geolocalización`)
+            }
+        )
+    }
+}
+localizacionInicial()
 
 // - Pintar los datos en la card de clima
 // - Activar el buscador por ciudades
 // - Geolocation de JS
+
+function pintarCardClima(clima) {
+    climaNombre.innerText = clima.name;
+    climaTemp.innerHTML = `${clima.main.temp}<span>°C</span>`;
+
+    const icon = clima.weather[0].icon
+
+    climaIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" >`
+}
+
+inputWeather.addEventListener('keyup', (evento) => {
+
+    if(evento.key === 'Enter') {
+
+        obtenerClimaPorCiudad()
+
+    }
+
+})
+
+
+function obtenerClimaPorCiudad() {
+
+    const ciudad = inputWeather.value;
+
+    axios.get(`${URL}&q=${ciudad}`)
+                .then(respuesta => {
+
+                    pintarCardClima(respuesta.data)
+
+                }).catch((error) => console.warn(`Error al obtener ciudad`))
+}
+
+searchBtn.addEventListener('click', obtenerClimaPorCiudad)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // fetch(URL, { method: 'GET' })
